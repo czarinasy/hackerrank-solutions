b727ef35d17e5d01415601948325bddfe0b93e25
czarina.sy <czarina.sy@rendernetworks.com>
2023-07-10 15:11:51 +0800

    Week 2, Day 4: Diagonal Difference

---
+++ 3_month_preparation_kit/week_2/day_4_diagonal_difference.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+
+    size = len(arr)
+
+    l2r = []
+    r2l = []
+
+    for i in range(size):
+        l2r.append(arr[i][i])
+        r2l.append(arr[i][size - 1 - i])
+
+    return abs(sum(l2r) - sum(r2l))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
