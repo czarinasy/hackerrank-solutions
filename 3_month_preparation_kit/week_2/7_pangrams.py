3b056c5c2399ed5db18c471f42e0d4e20c7fb8d7
czarina.sy <czarina.sy@rendernetworks.com>
2023-07-19 10:52:41 +0800

    Added misssing problem

---
+++ 3_month_preparation_kit/week_2/7_pangrams.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    s_stripped = ''.join(s.split()).lower()
+    s_set = set(s_stripped)
+
+    alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z'}
+
+    if s_set == alphabet:
+        return 'pangram'
+    return 'not pangram'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
