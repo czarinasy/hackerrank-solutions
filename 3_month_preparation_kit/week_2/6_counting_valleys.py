e4dae8cd99d0a53b50b8bff4cd46be007f641ba3
czarina.sy <czarina.sy@rendernetworks.com>
2023-07-19 10:52:16 +0800

    Updated to correct problem

--- 3_month_preparation_kit/week_2/6_counting_valleys.py
+++ 3_month_preparation_kit/week_2/6_counting_valleys.py
@@ -8,31 +8,46 @@ import sys
 
 
 #
-# Complete the 'pangrams' function below.
+# Complete the 'countingValleys' function below.
 #
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
 #
 
-def pangrams(s):
+def countingValleys(steps, path):
     # Write your code here
-    s_stripped = ''.join(s.split()).lower()
-    s_set = set(s_stripped)
 
-    alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+    cur_level = 0
 
-    if s_set == alphabet:
-        return 'pangram'
-    return 'not pangram'
+    prev_level = -1
+
+    valley_counter = 0
+
+    for step in path:
+        prev_level = cur_level
+
+        if step == 'D':
+            cur_level -= 1
+        elif step == 'U':
+            cur_level += 1
+
+        if (prev_level == 0) and (cur_level == -1):
+            valley_counter += 1
+
+    return valley_counter
 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = input()
+    steps = int(input().strip())
 
-    result = pangrams(s)
+    path = input()
 
-    fptr.write(result + '\n')
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
 
     fptr.close()
